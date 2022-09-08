018146e31022d2f4235ebeb8be33c83dfe8550f8
김강현 <101877146+kimgh06@users.noreply.github.com>
2022-09-08 22:44:56 +0900

    Update 포털보조.js

--- 포털보조.js
+++ 포털보조.js
@@ -1,21 +1,25 @@
 let searchLine = document.querySelector("#searchLine");
 
 function toggleMenu(){
-    const menu = document.querySelector("#menu");
+    const mainside = document.querySelector("#mainside");
     const menuButton = document.querySelector("#menuButton");
     const sideBar = document.querySelector("#sideBar");
-    if(menu.classList.contains("hidden")){
-        menu.classList.remove("hidden");
+    const loginDiv = document.querySelector("#loginDiv");
+    if(mainside.classList.contains("hidden")){
+        mainside.classList.remove("hidden");
         menuButton.style.backgroundColor = "white";
         menuButton.style.color = "black";
         menuButton.style.transform = "rotate(-180deg)"
         sideBar.style.backgroundColor = "rgb(105, 191, 225)";
+        loginDiv.classList.remove("hidden");
     }
     else{
-        menu.classList.add("hidden");
+        mainside.classList.add("hidden");
         menuButton.style.color = "white";
         menuButton.style.backgroundColor = "rgb(105, 191, 225)";
         menuButton.style.transform = "rotate(0deg)"
         sideBar.style.backgroundColor = "rgba(255, 255, 255, 0)";
+        loginDiv.classList.add("hidden");
     }
 }
+
