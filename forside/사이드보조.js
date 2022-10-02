73c95577f9835593e9ac91549ec4b77d0ee10159
kkh061101 <kkh061101@naver.com>
2022-10-02 14:54:29 +0900

    js loginDiv 부분 구현

--- forside/사이드보조.js
+++ forside/사이드보조.js
@@ -6,5 +6,20 @@ const inputPw = document.createElement("input");
 inputPw.id = "loginPw";
 inputPw.placeholder = "Password";
 inputPw.type = "text";
+const loginInputDiv = document.createElement("div");
+loginInputDiv.appendChild(inputId);
+loginInputDiv.appendChild(inputPw);
 
-console.log(inputId.id);
+const loginButton = document.createElement("button");
+loginButton.innerText = "login";
+loginButton.id = "loginButton";
+const buttonDiv = document.createElement("div");
+buttonDiv.appendChild(loginButton);
+
+const loginDiv = document.createElement("div");
+loginDiv.id = "loginDiv";
+loginDiv.style.display = "none";
+loginDiv.appendChild(loginInputDiv);
+loginDiv.appendChild(buttonDiv);
+
+document.querySelector("#sideBar").appendChild(loginDiv);
