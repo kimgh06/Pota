6359bf6be583fe7c44d4d80a7eda1340442b7670
kkh061101 <kkh061101@naver.com>
2022-10-02 14:31:29 +0900

    버튼 크기 수정

--- forside/사이드보조.js
+++ forside/사이드보조.js
@@ -1,2 +1,10 @@
-const input1 = document.createElement("input");
-console.log("따봉");
+const inputId = document.createElement("input");
+inputId.id = "loginId";
+inputId.placeholder = "Id";
+inputId.type = "text";
+const inputPw = document.createElement("input");
+inputPw.id = "loginPw";
+inputPw.placeholder = "Password";
+inputPw.type = "text";
+
+console.log(inputId.id);
