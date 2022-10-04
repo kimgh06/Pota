24520653fc90c7a523bb3decdd9e4470bee483ea
kkh061101 <kkh061101@naver.com>
2022-10-05 07:41:00 +0900

    이름 수정

--- 포털보조.js
+++ 포털보조.js
@@ -94,16 +94,16 @@ function search(event){
         console.log("없");
 }
 
-const loginButton = document.querySelector("#searchButton");
-loginButton.addEventListener("mouseover", searchButtonReactOn);
-loginButton.addEventListener("mouseout", searchButtonReactOff);
+const searchButton = document.querySelector("#searchButton");
+searchButton.addEventListener("mouseover", searchButtonReactOn);
+searchButton.addEventListener("mouseout", searchButtonReactOff);
 
 function searchButtonReactOn(){
-    loginButton.style.backgroundColor = 'rgb(123, 242, 202)';
+    searchButton.style.backgroundColor = 'rgb(123, 242, 202)';
 }
 
 function searchButtonReactOff(){
-    loginButton.style.backgroundColor = 'aquamarine';
+    searchButton.style.backgroundColor = 'aquamarine';
 }
 
 const searchForm = document.querySelector("#searchForm");
