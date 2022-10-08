7ddf5d35ddb75451fd7b40327f7bcb5457373929
kkh061101 <kkh061101@naver.com>
2022-10-08 22:14:48 +0900

    Update login.js

--- etcs/login.js
+++ etcs/login.js
@@ -14,6 +14,10 @@ function pwPlaceholderFocusOff(){
     pw.placeholder = 'password';
 }
 
+function login(){
+    alert("아직 안돼요");
+}
+
 const id = document.querySelector("#id");
 id.addEventListener("focusin", idPlaceholderFocusOn);
 id.addEventListener("focusout", idPlaceholderFocusOff);
@@ -21,3 +25,6 @@ id.addEventListener("focusout", idPlaceholderFocusOff);
 const pw = document.querySelector("#pw");
 pw.addEventListener("focusin", pwPlaceholderFocusOn);
 pw.addEventListener("focusout", pwPlaceholderFocusOff);
+
+const submitButton = document.querySelector("#submitButton");
+submitButton.addEventListener("click", login);
